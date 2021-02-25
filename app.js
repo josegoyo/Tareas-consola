7aa462a3998edbd3bb2ad30f31602ed7a5c015e6
Jose Gregorio Calderon <jgregorio2791@gmail.com>
2021-02-25 11:24:44 -0800

    nuevas funciones

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('colors');
-const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
+const { inquirerMenu, pausa } = require('./helpers/inquirer');
 console.clear();
 
 const main = async () => {
@@ -9,13 +9,15 @@ const main = async () => {
 
     do {
 
-        opt = await mostrarMenu();
-        console.log({ opt })
-        await pausa();
+        opt = await inquirerMenu();
+        console.log( opt )
+
+        if(opt !== '0')
+            await pausa();
 
     } while (opt !== '0');
 
-    //pausa();
+    
 }
 
 
---
+++ helpers/inquirer.js
@@ -0,0 +1,71 @@
+const inquirer = require('inquirer');
+require('colors');
+
+const menuOpt = [
+    {
+        type: 'list',
+        name: 'opcion',
+        message: '¿Qué deseas hacer?',
+        choices: [
+            {
+                value: '1',
+                name: '1. Crear tarea'
+            },
+            {
+                value: '2',
+                name: '2. Listar tarea'
+            },
+            {
+                value: '3',
+                name: '3. Listar tareas completas'
+            },
+            {
+                value: '4',
+                name: '4. Listar tareas pendientes'
+            },
+            {
+                value: '5',
+                name: '5. Completar tareas'
+            },
+            {
+                value: '6',
+                name: '6. Borrar tareas'
+            },
+            {
+                value: '0',
+                name: '0. Salir'
+            }
+        ]
+    }
+]
+
+const question = [
+    {
+        type: 'input',
+        name: 'enter',
+        message: "Presione ENTER para seguir:",
+    }
+]
+
+const inquirerMenu = async () => {
+    console.clear();
+    console.log('====================='.green);
+    console.log('Seleccione una opción'.green);
+    console.log('=====================\n'.green);
+
+    const { opcion } = await inquirer.prompt(menuOpt)
+    return opcion;
+}
+
+
+const pausa = async () => {
+
+    const { enter } = await inquirer.prompt(question)
+    return enter;
+
+}
+
+module.exports = {
+    inquirerMenu,
+    pausa
+}
